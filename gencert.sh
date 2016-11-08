88a82099a4ac16e7ec340c4f3a658111a49c3239
Leo <leononame@lho.io>
2016-11-08 18:20:00 +0100

    Add shitty self signed certificate for https

---
+++ gencert.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -newkey rsa:4096 -keyout private.key -out public.cert -days 365 -nodes
--- server.js
+++ server.js
@@ -6,9 +6,11 @@
 // SETUP
 // =================================================
 // =================================================
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 443;
 var morgan = require('morgan'); // Logger
 
 // =================================================
@@ -16,11 +18,24 @@ var morgan = require('morgan'); // Logger
 // CONFIGURATION
 // =================================================
 // =================================================
-
+// -------------------------------------------------
+// HTTP Server for asking users to redirect to HTTPS
+// -------------------------------------------------
+var http = require('http');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Please use https!\n");
+});
+server.listen(process.env.HTTPPORT || 80);
 // -------------------------------------------------
 // General configuration
 // -------------------------------------------------
 app.use(morgan('dev')); 	// Start logger
+// HTTPS certificates
+var options = {
+   key  : fs.readFileSync(__dirname + '/private.key'),
+   cert : fs.readFileSync(__dirname + '/public.cert')
+};
 // -------------------------------------------------
 // Views
 // -------------------------------------------------
@@ -36,6 +51,6 @@ require('./app/routes.js')(app);
 // STARTUP
 // =================================================
 // =================================================
-app.listen(port,  function () {
-  console.log('ISM Device server running on port ' + port + '...');
+https.createServer(options, app).listen(port, function () {
+   console.log('ISM Device server running on port ' + port + '...');
 });

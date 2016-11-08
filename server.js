7f0ae233fc470dd19e4a430fe0fcf4460b772549
Leo <leononame@lho.io>
2016-11-08 16:03:20 +0100

    Add Bootstrap and JQuery

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use(morgan('dev')); 	// Start logger
 // Views
 // -------------------------------------------------
 app.set('view engine', 'pug');
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.set('views', __dirname+'/views');
 require('./app/routes.js')(app);
 

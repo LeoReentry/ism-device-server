0a1fb258702b4a4de17b93ad553a6e656c9a4293
Leo <leononame@lho.io>
2016-11-09 10:15:12 +0100

    Add fetching data from API

--- app/routes.js
+++ app/routes.js
@@ -25,4 +25,31 @@ module.exports = function(app) {
       pagetitle: 'About'
     });
   });
+  // =================================================
+  // =================================================
+  // Config PAGE
+  // =================================================
+  // =================================================
+  app.get('/configuration', function(req, res) {var request = require('request');
+    var url = 'https://ismportal.azurewebsites.net/api/newdevice';
+    request.get({
+        url: url,
+        json: true
+      }, (err, resp, data) => {
+        if (err) {
+          console.log('Error:', err);
+          res.render('err');
+        } else if (res.statusCode !== 200) {
+          console.log('Status:', res.statusCode);
+          res.render('err');
+        } else {
+          // data is already parsed as JSON:
+          res.render('configuration', {
+            pagetitle: 'Configuration',
+            data: data
+          });
+        }
+    });
+  });
+
 };
